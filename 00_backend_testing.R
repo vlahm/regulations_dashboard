4012c130abbe26ca9897dac14bfab153cd62a25c
vlahm <vlahm13@gmail.com>
2017-01-15 11:58:39 -0800

    simplified a few things, added notes

--- 00_backend_testing.R
+++ 00_backend_testing.R
@@ -3,6 +3,9 @@
 #Contact: vlahm13@gmail.com
 #Creation Date: 12/7/16
 
+#note: if print() fails with "$ operator is invalid for atomic vectors",
+#use paste() or cat() instead
+
 #setup
 
 if (!require("curl")) install.packages("curl")
@@ -16,9 +19,9 @@ library(federalregister)
 
 #topic search
 x <- fr_search(term='fisheries', per_page=100, order='newest')
-cat(x$results$title[1:10], sep='\n\n') #first ten titles by most recent
-cat(x$results$publication_date[1:10]) #dates
-cat(x$results$abstract[1]) #first abstract
+print(x$results$title[1:10]) #first ten titles by most recent
+print(x$results$publication_date[1:10]) #dates
+print(x$results$abstract[1]) #first abstract
 
 #title search?
 
@@ -30,7 +33,7 @@ x <- fr_search(term='arctic', agencies='USGS')
 
 #order by comments close date
 x <- fr_search(term='fisheries', per_page=100, order='newest', fields='comments_close_on')
-dates <- paste(x$results$comments_close_on)
+dates <- x$results$comments_close_on
 sort(dates[which(dates!='NA')], decreasing=TRUE)
 
 #issue 1: these dates do not correspond to the dates on the dashboard
@@ -38,5 +41,13 @@ sort(dates[which(dates!='NA')], decreasing=TRUE)
 #it may be that a lot of the recent days are showing up as NA
 
 #issue 2: some of the titles currently on the dashboard doesn't show up here.
-x <- fr_search(term='fisheries')
-paste(unique(x$results$title))
+x <- fr_search(term='fisheries', version='v4')
+unique(x$results$title)
+
+
+#other potentially useful functions
+fr_get
+pi_search
+pi_get
+pi_current
+fr_agencies
