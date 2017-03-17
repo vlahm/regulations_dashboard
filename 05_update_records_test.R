b05031c7845dc83c45fb6c166efba5a29c9e7332
mike <vlahm@uw.edu>
2017-03-16 18:11:41 -0700

    finding sheets delete/update workaround

--- 05_update_records_test.R
+++ 05_update_records_test.R
@@ -5,19 +5,25 @@
 #Creation Date: 3/12/17
 rm(list=ls()); cat('\014') #clear everything
 
-#TODO: upload/edit speed comparison
-#dedicated google account
-
 #INSTRUCTIONS:
 #set the working directory to the place where you want to store a CSV copy of current records
-setwd('C:/Users/Mike/Desktop/fr_test')
-#03_functions.R should go in there. 04_update_records.R can go anywhere.
-#Google Sheets cache information will also be stored there in a file called .httr-oauth
-#any old fedRegOut.csv files will now be obsolete, so you can delete those.
-#most recent search date will also be stored.
-deepSearch = 150 #choose search depth (days before today) for the first run
-lookBack = 7 #choose depth to check every time, to capture late additions
+    setwd('C:/Users/Mike/Desktop/fr_test')
+    #03_functions.R should go in there. 04_update_records.R can go anywhere.
+    #Google Sheets cache information will also be stored there in a file called .httr-oauth
+    #any old fedRegOut.csv files will now be obsolete, so you can delete those.
+    #most recent search date will also be stored.
+#choose search depth (days before today) for the first run
+    deepSearch = 150
+#choose depth to check every time, to capture late additions
+    lookBack = 7 
 #then source this file. Nothing else needs to be edited, but feel free to customize.
+
+#TROUBLESHOOTING ON WINDOWS
+#if you get curl error: "Operation was aborted by an application callback", update devtools with:
+    #remove.packages('devtools'); install.packages('devtools')
+#if you get curl error: "Stream error in the HTTP/2 framing layer", update curl with:
+    #install.packages("https://github.com/jeroen/curl/archive/master.tar.gz", repos = NULL)
+    #(you'll need to have Rtools installed)
 
 #start timing
 ptm = proc.time()
@@ -77,13 +83,17 @@ if(noPrevRun | noRecords){
 } else {
     message(paste0('Last run date: ',lastRun,'. (Re)retrieving records published after ',startDate,'.'))
 }
-span = todayRaw - as.Date(startDate, format='%m/%d/%y', tz='PST')
-nchunks = ceiling(as.numeric(span)/30)
+span = as.numeric(todayRaw - as.Date(startDate, format='%m/%d/%y', tz='PST'))
+nchunks = ceiling(span/30)
 newRegs = NULL
-for(i in nchunks:1){
-    chunkStart = format.Date(todayRaw-(i*30), format='%m/%d/%y', tz='PST')
-    chunkEnd = format.Date(todayRaw-((i-1)*30), format='%m/%d/%y', tz='PST')
-    newRegs = rbind.fill(newRegs, regSearch1(start=chunkStart, end=chunkEnd))
+if(nchunks > 1){
+    for(i in nchunks:1){
+        chunkStart = format.Date(todayRaw-(i*30), format='%m/%d/%y', tz='PST') #NOTICE: ends up grabbing more than deepSearch records
+        chunkEnd = format.Date(todayRaw-((i-1)*30), format='%m/%d/%y', tz='PST')
+        newRegs = rbind.fill(newRegs, regSearch1(start=chunkStart, end=chunkEnd))
+    }
+} else {
+    newRegs = rbind.fill(newRegs, regSearch1(start=startDate, end=today))
 }
 
 #combine records
@@ -91,13 +101,11 @@ allRegs = rbind.fill(oldRegs, newRegs)
 
 #remove duplicates, unusable records, and those closed for comment
 dups = duplicated(paste(allRegs$title, allRegs$comment_url))
-allRegs = filter(allRegs, comments_close_on != '', comment_url != '', !dups,
-                 as.Date(comments_close_on,format='%m/%d/%y',tz='PST') > todayRaw)
+allRegs = filter(allRegs, comments_close_on != '', !dups,
+                 as.Date(comments_close_on,format='%m/%d/%Y',tz='PST') > todayRaw)
 
 #write all records to local file
 write.csv(allRegs, 'fedRegOut.csv', row.names=FALSE)
-
-
 
 #update google sheet. docs recommend delete-rewrite as the fastest method,
 #but it's worth testing gs_edit_cells() and gs_add_row().
@@ -106,18 +114,21 @@ write.csv(allRegs, 'fedRegOut.csv', row.names=FALSE)
 #     dash = gs_title('fedRegDash')
 #     gs_delete(dash)
 # }
-# gs_upload('fedRegOut.csv', sheet_title='fedRegDash')
 
-#create new google sheet if this is first run
-if(!'fedRegDash' %in% gs_ls()$sheet_title){
-    message('Creating new Google Sheet; populating with all records')
-    sht = gs_new('fedRegDash', input=allRegs, trim=TRUE, verbose=FALSE)
+#create google sheet if this is first run
+if(!'regDash' %in% gs_ls()$sheet_title){
+    message('Creating new Google Sheet. Populating with all records.')
+    gs_upload('fedRegOut.csv', sheet_title='regDash')
+    # dash = gs_new('fedRegDash', input=allRegs, trim=TRUE, verbose=FALSE)
 }
 
-#remove old records from google sheet and add new records
-gs_edit_cells(sht, 
+#update google sheet with new records
+dash = gs_title('regDash')
+gs_edit_cells(dash, input=allRegs, trim=TRUE)
+dash = gs_ws_rename(dash, from='regDash', to='temp')
+dash = gs_ws_new(dash, ws_title='regDash', input=allRegs, trim=TRUE, verbose=FALSE)
+dash = gs_ws_delete(dash, ws='temp')
 
 
 runTime = proc.time() - ptm
 message(paste('Run completed in',round(runTime[3]/60,2),'minutes.'))
-
